4df16d659fdde69e256648cd5571d66585a2c513
Martinfree <lvarchenko99@gmail.com>
2020-11-19 15:35:46 +0200

    update models

--- tracker/board/models.py
+++ tracker/board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Task(models.Model):
+    head = models.CharField(max_length=32,blank=False,unique=False)
+    description = models.CharField(max_length=256,blank=True)
+    release_date = models.DateTimeField(auto_now_add=timezone.now(),editable=False)
+    edit_at = models.DateTimeField(auto_now=timezone.now())
+    status = models.BooleanField(default=False)
+    board = models.ForeignKey("Board",to_field="id",on_delete=models.SET_NULL,null=True,blank=True)
+
+class Board(models.Model):
+    name = models.CharField(max_length=32)
+    release_date = models.DateTimeField(auto_now_add=timezone.now(),editable=False)
+    edit_at = models.DateTimeField(auto_now=timezone.now())
+
